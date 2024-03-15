5af51fc58c99df77de7e7e730e30bb2339485c12
Connor <concon477@virginmedia.com>
2024-03-15 19:39:42 +0000

    maybe that

--- Proxy/build.gradle.kts
+++ Proxy/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    implementation ("com.github.utfunderscore.ApiWrapper:Matchmaker:latest")
+    implementation ("com.github.utfunderscore.MatchMaker:ApiWrapper:latest")
 
     implementation("co.aikar:acf-velocity:0.5.1-SNAPSHOT")
 
