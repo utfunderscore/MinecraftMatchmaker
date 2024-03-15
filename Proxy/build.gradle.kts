85aee45862a7e560b99ff3dac3b8b71d9961b565
Connor <concon477@virginmedia.com>
2024-03-15 19:35:23 +0000

    fix jitpack import

--- Proxy/build.gradle.kts
+++ Proxy/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    implementation("com.github.readutf.MatchMaker:api-wrapper:+")
+    implementation ("com.github.utfunderscore.ApiWrapper:Matchmaker:latest")
 
     implementation("co.aikar:acf-velocity:0.5.1-SNAPSHOT")
 
